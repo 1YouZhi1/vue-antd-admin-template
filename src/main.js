4a8a690279868f6a4c6bd44c7b7702b86cf374d3
ShengXuan <2504658803@qq.com>
2024-01-08 12:48:21 +0800

    store和auth

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // 导入路由
-//import store from './store';
+import store from './store/index';
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 
 createApp(App)
-  //.use(store)
+  .use(store)
   .use(router) // 使用路由
   .use(Antd) // 使用 Ant Design Vue
   .mount('#app');
---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex'
+import user from './modules/user'
+import getters from './getters'
+
+
+const store = createStore({
+    modules: {
+        user
+    },
+    getters
+})
+
+export default store
